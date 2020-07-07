523c93aa6bbd050be15700f4a8b64f03de50cabc
Kieron <kieronboz@gmail.com>
2020-07-07 20:07:05 +0100

    register

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -7,32 +7,64 @@
       >
         <h1 class="text-indigo-100 text-3xl">Register</h1>
         <div class="w-full max-w-xs">
-          <a href="https://api.comps.gg/connect/discord" class="link my-5 py-5"><button v-on:click="greet" class="bg-indigo-200 text-black" type="button" style="width: 100%;"><i class="fa fa-discord"></i>Discord</button></a>
-  <form class="hidden bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-    <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-        Username
-      </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
-    </div>
-    <div class="mb-6">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-        Password
-      </label>
-      <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
-      <p class="text-red-500 text-xs italic">Please choose a password.</p>
-    </div>
-    <div class="flex items-center justify-between">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-        Sign In
-      </button>
-      <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-        Forgot Password?
-      </a>
-    </div>
-  </form>
- 
-</div>
+          <a href="https://api.comps.gg/connect/discord" class="link my-5 py-5"
+            ><button
+              v-on:click="greet"
+              class="bg-indigo-200 text-black"
+              type="button"
+              style="width: 100%;"
+            >
+              <i class="fa fa-discord"></i>Discord
+            </button></a
+          >
+          <form class="hidden bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div class="mb-4">
+              <label
+                class="block text-gray-700 text-sm font-bold mb-2"
+                for="username"
+              >
+                Username
+              </label>
+              <input
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Username"
+              />
+            </div>
+            <div class="mb-6">
+              <label
+                class="block text-gray-700 text-sm font-bold mb-2"
+                for="password"
+              >
+                Password
+              </label>
+              <input
+                class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="******************"
+              />
+              <p class="text-red-500 text-xs italic">
+                Please choose a password.
+              </p>
+            </div>
+            <div class="flex items-center justify-between">
+              <button
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Sign In
+              </button>
+              <a
+                class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                href="#"
+              >
+                Forgot Password?
+              </a>
+            </div>
+          </form>
+        </div>
       </div>
     </section>
     <mailChimp />
@@ -53,13 +85,25 @@ export default {
   },
   methods: {
     greet: function() {
-        fetch(`https://api.comps.gg/connect/discord`)
-          .then((response) => response.json())
-          .then((data) => {
-            console.log(data)
-          })
-          .catch((error) => console.error(error));
-    }
+      fetch(`https://api.comps.gg/connect/discord`, {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(), // body data type must match "Content-Type" header
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => console.error(error));
+    },
   },
   metaInfo() {
     return {
