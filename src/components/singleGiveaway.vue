907a81b60c623b2b6387ab555d30ed30541533bb
Kieron <kieronboz@gmail.com>
2020-07-12 15:33:23 +0100

    button restyle timeout increase

--- src/components/singleGiveaway.vue
+++ src/components/singleGiveaway.vue
@@ -656,7 +656,7 @@ function gleamChecker() {
       childSpan.innerHTML +=
         "<svg class='inline w-5 h-5 ml-2' fill='none' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' viewBox='0 0 24 24' stroke='currentColor'><path d='M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14'></path></svg>";
     });
-  }, 750);
+  }, 1100);
 }
 </script>
 
