193b40ce740e94c162e2dc0698ee94c590fec497
kieron <kieronboz@gmail.com>
2021-02-04 22:42:10 +0000

    change privacy policy

--- src/views/PrivacyPolicy.vue
+++ src/views/PrivacyPolicy.vue
@@ -6,11 +6,7 @@
       >
         <h1 class="text-3xl text-indigo-100">Privacy Policy</h1>
         <p class="mb-3">
-          Here at Comps.gg we use Ezoic to provide personalization and analytic
-          services on this website, as such Ezoic's privacy policy is in effect
-          and can be reviewed
-          <a target="_blank" href="https://g.ezoic.net/privacy/comps.gg">here</a
-          >.
+          Coming Soon.
         </p>
       </div>
     </section>
