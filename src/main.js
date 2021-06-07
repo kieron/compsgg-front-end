cb8718f9a34bb215acac17acd457dd1311cdd7e3
kieron <kieronboz@gmail.com>
2021-06-07 14:26:35 +0100

    reinstate google analytics

--- src/main.js
+++ src/main.js
@@ -1,25 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-//import VueGtag from "vue-gtag";
-import SvgIcon from './util/SvgIcon.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import VueGtag from "vue-gtag";
+import SvgIcon from "./util/SvgIcon.vue";
 
-import '@/assets/css/tailwind.css'
+import "@/assets/css/tailwind.css";
 
-// if (process.env.NODE_ENV === 'production') {
-//     Vue.use(VueGtag, {
-//         config: { id: "UA-84362939-10" }
-//     });
-// }
+if (process.env.NODE_ENV === "production") {
+  Vue.use(VueGtag, {
+    config: { id: "UA-84362939-10" },
+  });
+}
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.component('SvgIcon', SvgIcon)
+Vue.component("SvgIcon", SvgIcon);
 
 new Vue({
-    router, store,
-    render: h => h(App),
-
-}).$mount('#app')
-
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
