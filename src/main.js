231536d12fb140fb08aab19421e8b953eb2ba5d7
kieron <kieronboz@gmail.com>
2021-10-07 13:26:35 +0100

    adding ads because im evil

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import store from "./store";
 //import VueGtag from "vue-gtag";
 import SvgIcon from "./util/SvgIcon.vue";
-
+import Ads from "vue-google-adsense";
 import "@/assets/css/tailwind.css";
 
 // if (process.env.NODE_ENV === 'production') {
@@ -12,7 +12,8 @@ import "@/assets/css/tailwind.css";
 //         config: { id: "UA-84362939-10" }
 //     });
 // }
-
+Vue.use(require("vue-script2"));
+Vue.use(Ads.AutoAdsense, { adClient: "7677794196959549", isNewAdsCode: true });
 Vue.config.productionTip = false;
 
 Vue.component("SvgIcon", SvgIcon);
