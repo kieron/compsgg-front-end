aa1c3a2b108141455c1d2606dfbcfdc6efdd459a
kieron <kieronboz@gmail.com>
2021-10-20 14:23:03 +0100

    hide mailchimp for more ads because again im evil

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,20 +1,21 @@
 <template>
-	<div class="default-layout">
-		<NavBar></NavBar>
-		<router-view></router-view>
-		<MailChimp />
-		<Footer />
-	</div>
+  <div class="default-layout">
+    <NavBar></NavBar>
+    <router-view></router-view>
+    <!-- <MailChimp /> -->
+    <Footer />
+  </div>
 </template>
 
 <script>
-import NavBar from '@/components/navBar'
-import Footer from '@/components/footerMain'
-import MailChimp from '@/components/mailChimp'
+import NavBar from "@/components/navBar";
+import Footer from "@/components/footerMain";
+//import MailChimp from '@/components/mailChimp'
 export default {
-	name: 'DefaultLayout',
-	components: {
-		NavBar, Footer, MailChimp
-	}
-}
+  name: "DefaultLayout",
+  components: {
+    NavBar,
+    Footer,
+  },
+};
 </script>
