9290fa9e3e54b34465a14e0be528511be1088985
kieron <kieronboz@gmail.com>
2021-10-20 12:21:27 +0100

    try newadcode false for adsense

--- src/main.js
+++ src/main.js
@@ -2,18 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-//import VueGtag from "vue-gtag";
 import SvgIcon from "./util/SvgIcon.vue";
 import Ads from "vue-google-adsense";
 import "@/assets/css/tailwind.css";
 
-// if (process.env.NODE_ENV === 'production') {
-//     Vue.use(VueGtag, {
-//         config: { id: "UA-84362939-10" }
-//     });
-// }
 Vue.use(require("vue-script2"));
-Vue.use(Ads.AutoAdsense, { adClient: "7677794196959549", isNewAdsCode: true });
+Vue.use(Ads.AutoAdsense, { adClient: "7677794196959549", isNewAdsCode: false });
 Vue.config.productionTip = false;
 
 Vue.component("SvgIcon", SvgIcon);
