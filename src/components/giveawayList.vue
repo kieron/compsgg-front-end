584351654ac2500fc3c9482515a074a28a9ea3eb
kieron <kieronboz@gmail.com>
2022-01-27 13:52:41 +0000

    FIX LAYOUT remove deps

--- src/components/giveawayList.vue
+++ src/components/giveawayList.vue
@@ -45,8 +45,8 @@
             id="filter"
             v-if="showToggle"
           >
-            <div class="flex">
-              <div class="w-full px-3 mb-6 md:w-1/3 md:mb-0">
+            <div class="flex flex-wrap">
+              <div class="w-1/2 px-3 mb-6 md:w-1/3 md:mb-0">
                 <label
                   class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase "
                   for="grid-state"
@@ -79,7 +79,7 @@
                   </div>
                 </div>
               </div>
-              <div class="w-full px-3 mb-6 md:w-1/3 md:mb-0">
+              <div class="w-1/2 px-3 mb-6 md:w-1/3 md:mb-0">
                 <label
                   class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase "
                   for="grid-state"
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import SvgIcon from "./util/SvgIcon.vue";
 import Ads from "vue-google-adsense";
 import "@/assets/css/tailwind.css";
 
-Vue.use(require("vue-script2"));
+// Vue.use(require("vue-script2"));
 Vue.use(Ads.AutoAdsense, {
   adClient: "pub-7677794196959549",
   isNewAdsCode: false,
