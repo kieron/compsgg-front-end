deb6a5c828925ff84592c79dce3f4b0179094a4d
kieron <kieronboz@gmail.com>
2022-02-10 10:44:45 +0000

    rm mailchimp

--- src/components/mailChimp.vue
+++ src/components/mailChimp.vue
@@ -1,12 +1,12 @@
 <template>
   <section class="flex bg-gray-100 md:bg-middle">
     <div
-      class="flex flex-col max-w-4xl mx-auto my-10 overflow-hidden bg-gray-100 md:h-56 md:bg-white lg:rounded-lg md:shadow-lg md:flex-row"
+      class="flex flex-col max-w-4xl mx-auto my-10 overflow-hidden bg-gray-100  md:h-56 md:bg-white lg:rounded-lg md:shadow-lg md:flex-row"
     >
       <div class="items-center justify-center md:flex md:w-1/2 md:bg-gray-700">
         <div class="px-8 py-6 md:py-0">
           <h2
-            class="text-2xl font-medium tracking-widest text-gray-700 md:text-gray-100"
+            class="text-2xl font-medium tracking-widest text-gray-700  md:text-gray-100"
           >
             Never miss a chance to win!
           </h2>
@@ -17,7 +17,7 @@
         </div>
       </div>
       <div
-        class="flex items-center justify-center pb-6 border-gray-700 md:py-0 md:w-1/2 md:border-b-8 md:bg-gray-100"
+        class="flex items-center justify-center pb-6 border-gray-700  md:py-0 md:w-1/2 md:border-b-8 md:bg-gray-100"
       >
         <mailchimp-subscribe
           url="https://comps.us10.list-manage.com/subscribe/post-json"
@@ -34,12 +34,12 @@
                 >
                   <input
                     placeholder="Enter your email"
-                    class="px-4 py-3 text-gray-800 placeholder-gray-700 bg-gray-200 border-2 border-gray-300 outline-none focus:bg-gray-100"
+                    class="px-4 py-3 text-gray-800 placeholder-gray-700 bg-gray-200 border-2 border-gray-300 outline-none  focus:bg-gray-100"
                     type="email"
                     @input="setEmail($event.target.value)"
                   />
                   <button
-                    class="px-4 py-3 font-semibold tracking-widest text-gray-100 uppercase bg-gray-700 md:rounded-r-md hover:bg-gray-600"
+                    class="px-4 py-3 font-semibold tracking-widest text-gray-100 uppercase bg-gray-700  md:rounded-r-md hover:bg-gray-600"
                     id="mailSubmit"
                     type="submit"
                   >
@@ -61,11 +61,11 @@
 </template>
 
 <script>
-import MailchimpSubscribe from "vue-mailchimp-subscribe";
+// import MailchimpSubscribe from "vue-mailchimp-subscribe";
 
 export default {
   components: {
-    MailchimpSubscribe,
+    // MailchimpSubscribe,
   },
   methods: {
     onError() {
